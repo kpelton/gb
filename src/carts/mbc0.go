e9041f6e0e1ade6a275b80b1b37d4e5dfc40a5f0
Kyle Pelton <kpelton@cecs.pdx.edu>
2013-08-28 08:43:03 -0700

    added new cart files

---
+++ src/carts/mbc0.go
@@ -0,0 +1,26 @@
+package carts
+
+import (
+	"fmt"
+)
+
+/////MBC0///////
+type MBC0 struct {
+	cart [0x8000]uint8
+}
+
+func NewMBC0(cart_data []uint8) *MBC0 {
+	m := new(MBC0)
+	copy(m.cart[:], cart_data)
+
+	return m
+}
+
+func (m *MBC0) Read_b(addr uint16) uint8 {
+	return m.cart[addr]
+}
+
+func (m *MBC0) Write_b(addr uint16, val uint8) {
+	fmt.Printf("WRITE TO ROM FAILED!!\n")
+}
+
---
+++ src/carts/mbc2.go
@@ -0,0 +1,45 @@
+package carts
+
+import (
+	"fmt"
+
+)
+
+
+/////MBC2///////
+type ROM_MBC2 struct {
+	cart [0x200000]uint8
+	bank uint16
+}
+
+func NewROM_MBC2(cart_data []uint8, size int) *ROM_MBC2 {
+	m := new(ROM_MBC2)
+	fmt.Println(size)
+	copy(m.cart[:], cart_data)
+	return m
+}
+
+func (m *ROM_MBC2) Read_b(addr uint16) uint8 {
+	var retval uint8
+
+	if addr < 0x4000 {
+		//always ROM bank #0
+		retval = m.cart[addr]
+	} else if addr < 0xc000 {
+		retval = m.cart[uint32(addr)+(uint32(m.bank)*0x4000)]
+	}
+	return retval
+}
+
+func (m *ROM_MBC2) Write_b(addr uint16, val uint8) {
+	if addr > 0x2000 && addr < 0x4000 {
+		if val > 1 {
+
+			//fmt.Println("Bank from",m.bank,val-1)
+			m.bank = uint16(val - 1)
+		} else {
+			m.bank = uint16(0)
+		}
+	}
+
+}
