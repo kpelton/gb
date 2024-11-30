29dcd7458f243a4c3ec0c51edf60254a64cde273
Kyle Pelton <peltonkyle@gmail.com>
2024-11-30 15:36:55 -0600

    gb/gb: Convert to modules
    Signed-off-by: Kyle Pelton <peltonkyle@gmail.com>

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export GO="/home/kyle/Downloads/go/bin/go"
-export GOPATH=$(pwd) 
-export PATH=$PATH:$GOOROOT/bin
-export GO111MODULE="off"
-$GO install cpu ;$GO build main.go
-#go install cpu; go build main.go
---
+++ go.mod
@@ -0,0 +1,8 @@
+module gb
+
+go 1.18
+
+require (
+	github.com/tarm/goserial v0.0.0-20151007205400-b3440c3c6355 // indirect
+	github.com/veandco/go-sdl2 v0.4.40 // indirect
+)
